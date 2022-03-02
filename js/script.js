7fed077fb23cef5efc6aa30e8140d0bd174b175f
SunjidaAkter <sanjidabintesayid7@gmail.com>
2022-03-02 15:09:43 +0600

    revised-try-catch-added

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ const infoToggler = display => {
 // searching
 const searchPhone = async () => {
     const searchField = document.getElementById('search-field');
-    searchText = searchField.value;
+    searchText = toLowerCase(searchField.value);
     searchField.value = '';
     document.getElementById('phone-details').textContent = '';
     spinnerToggler('block');
@@ -39,10 +39,14 @@ const searchPhone = async () => {
     }
     else {
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-        console.log(url);
-        const res = await fetch(url);
-        const data = await res.json();
-        displaySearchResult(data.data.slice(0, 20));
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            displaySearchResult(data.data.slice(0, 20));
+        }
+        catch (error) {
+            console.log(error);
+        }
 
     }
 }
